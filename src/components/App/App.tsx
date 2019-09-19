bfbe031f84c28d25ace1af0768bb90b039df51cb
Fernando Macias <fernandom@motabilityoperations.co.uk>
2019-09-19 21:59:19 +0100

    Add initial configuration, favicon, and packages

---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+const App = () => (
+  <div className={styles.app}>
+    <h1>Chocolab</h1>
+  </div>
+);
+
+export default App;
