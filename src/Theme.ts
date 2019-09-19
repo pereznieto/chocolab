479a70d3704c2ea96f235d21f0228e399a1f43e5
Fernando Macias <fernandom@motabilityoperations.co.uk>
2019-09-20 00:01:48 +0100

    Add custom theme colour

---
+++ src/Theme.ts
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#3587A4',
+    },
+  },
+});
+
+export default theme;
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,24 @@
 import { Container } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import theme from '../../Theme';
 import Facts from '../Facts/Facts';
 import Home from '../Home/Home';
 import Nav from '../Nav/Nav';
 import styles from './App.module.scss';
 
 const App = () => (
-  <Router>
-    <Route path='/' component={Nav} />
-    <Container maxWidth='lg' className={styles.app}>
-      <h1 className={styles.title}>Chocolate Labradors 🐾</h1>
-      <Route exact path='/' component={Home} />
-      <Route path='/fun-facts' component={Facts} />
-    </Container>
-  </Router>
+  <ThemeProvider theme={theme}>
+    <Router>
+      <Route path='/' component={Nav} />
+      <Container maxWidth='lg' className={styles.app}>
+        <h1 className={styles.title}>Chocolate Labradors 🐾</h1>
+        <Route exact path='/' component={Home} />
+        <Route path='/fun-facts' component={Facts} />
+      </Container>
+    </Router>
+  </ThemeProvider>
 );
 
 export default App;
