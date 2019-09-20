53eaa8dffe0d5d284f5731dc52c9256a9484358c
Fernando Macias Pereznieto <f@pereznieto.name>
2019-09-21 00:07:37 +0100

    Updates

---
+++ precache-manifest.07f4f198454197d4450e38e1e0fb5164.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938b02e16bc474eeec5820ea55670305",
+    "url": "/chocolab/index.html"
+  },
+  {
+    "revision": "5aa03f403e89b3b3fa31",
+    "url": "/chocolab/static/css/main.743b0227.chunk.css"
+  },
+  {
+    "revision": "1baaa7d642d468020726",
+    "url": "/chocolab/static/js/2.c1b7a0b6.chunk.js"
+  },
+  {
+    "revision": "5aa03f403e89b3b3fa31",
+    "url": "/chocolab/static/js/main.422abfb5.chunk.js"
+  },
+  {
+    "revision": "211aef3191bb490630c9",
+    "url": "/chocolab/static/js/runtime-main.ffcb44c9.js"
+  },
+  {
+    "revision": "123d4dfc14383fd13133dabdd4cabec5",
+    "url": "/chocolab/static/media/cute_puppy.123d4dfc.jpeg"
+  },
+  {
+    "revision": "ec8bc058fa0514bbacead920fce8374e",
+    "url": "/chocolab/static/media/dog_1.ec8bc058.jpeg"
+  },
+  {
+    "revision": "51dfec12cbb4dd444937651ac743f1b3",
+    "url": "/chocolab/static/media/dog_2.51dfec12.jpeg"
+  },
+  {
+    "revision": "a62da64f0bd4736a4dd0322a5c983b84",
+    "url": "/chocolab/static/media/lab_1.a62da64f.jpeg"
+  },
+  {
+    "revision": "d2d05378c41dc7eb3a09fa3e1a2eb21b",
+    "url": "/chocolab/static/media/puppy_1.d2d05378.jpeg"
+  },
+  {
+    "revision": "8dd97117395acd7d88700be9da4cc898",
+    "url": "/chocolab/static/media/puppy_2.8dd97117.jpeg"
+  },
+  {
+    "revision": "1dcc5f712df37f5f411ab961802b1558",
+    "url": "/chocolab/static/media/puppy_3.1dcc5f71.jpeg"
+  },
+  {
+    "revision": "28833f52992f216bfe479e0fb2f801bf",
+    "url": "/chocolab/static/media/puppy_4.28833f52.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f4af76fd19b6ca52a007f371e1d9b92d.js"
+  "/chocolab/precache-manifest.07f4f198454197d4450e38e1e0fb5164.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chocolab/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
